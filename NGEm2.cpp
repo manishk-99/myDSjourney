841548648b7dbcd6d4cc1f09923ea7fde9fc2efd
Manish Kumar <66010479+manishk-99@users.noreply.github.com>
2020-09-04 15:15:02 +0530

    Add files via upload

---
+++ NGEm2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+//prints element and NGE pair for all elements of arr[] of size n
+void printNGE(int arr[], int n) {
+    stack<int>s;
+    //push the first element to the stack
+    s.push(arr[0]);
+
+    //iterate for rest of the elements
+    for(int i=1;i<n;i++) {
+        if(s.empty()) {
+            s.push(arr[i]);
+            continue;
+        }
+
+        //if stack is not empty, then pop an element from stack.If the poppeed
+        //element is smaller than next, then 
+        //a) print the pair
+        //b) keep popping while elements are smaller and stack is not empty
+        while(s.empty()==false && s.top()<arr[i]) {
+            cout<<s.top()<<" --> "<<arr[i]<<endl;
+            s.pop();
+        }
+
+        //push next to stack so that we can find next greater for it
+        s.push(arr[i]);
+    }
+    //after iterating over the loop, the remaining elements in stack
+    //do not have the next greater element, so print -1 for them
+    while(s.empty()== false) {
+        cout<<s.top()<<" --> "<<-1<<endl;
+        s.pop();
+    }
+}
+
+//driver program to test above function
+int main() {
+    int arr[]={11,13,21,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    printNGE(arr,n);
+    return 0;
+}
